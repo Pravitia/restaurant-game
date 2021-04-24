b21d41893ce03e34395e5c400c7adf2ed5ccb35c
aaronli-git <li.aaron36@gmail.com>
2021-04-23 23:19:58 -0700

    Adding new food to loot list

--- restaurant/Main.java
+++ restaurant/Main.java
@@ -1,7 +1,11 @@
 package restaurant;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList<Food> loot = new ArrayList<Food>();
+        loot.add(new Food(25, "5", "Steak", "steakPicture"));
     }
 }
