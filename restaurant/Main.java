974d861e6fddc391212e39ef4d27e0c5605a62ca
aaronli-git <li.aaron36@gmail.com>
2021-04-23 23:42:48 -0700

    Added foods to the list of loot

--- restaurant/Main.java
+++ restaurant/Main.java
@@ -7,5 +7,18 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Food> loot = new ArrayList<Food>();
         loot.add(new Food(25, 5, "Steak", "steakPicture"));
+        loot.add(new Food(18, 4, "Rotisserie Chicken", "chickenPicture"));
+        loot.add(new Food(1.50, 3, "Hotdog", "hotdogPicture"));
+        loot.add(new Food(15, 4, "Burger", "burgerPicture"));
+        loot.add(new Food(12, 4, "Pepperoni Pizza", "pizzaPicture"));
+        loot.add(new Food(10, 3, "Turkey Sandwich", "sandwichPicture"));
+        loot.add(new Food(3.5, 3, "Fries", "friesPicture"));
+        loot.add(new Food(14, 4, "Ramen", "ramenPicture"));
+        loot.add(new Food(16, 4, "Sushi", "sushiPicture"));
+        loot.add(new Food(35, 5, "Strawberry Cake", "cakePicture"));
+        loot.add(new Food(1, 3, "Cup Noodle", "noodlePicture"));
+        loot.add(new Food(13, 4, "Tacos", "tacoPicture"));
+        loot.add(new Food(2, 3, "Soda", "sodaPicture"));
+        loot.add(new Food(7, 3, "Milkshake", "milkshakePicture"));
     }
 }
