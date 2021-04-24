3b52ee3c99bc1b409ba33b6b230f2d34fe01fb8e
JordanMai <jordanmai927@gmail.com>
2021-04-23 23:21:28 -0700

    Update Main.java
    Changed String to int

--- restaurant/Main.java
+++ restaurant/Main.java
@@ -6,6 +6,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Food> loot = new ArrayList<Food>();
-        loot.add(new Food(25, "5", "Steak", "steakPicture"));
+        loot.add(new Food(25, 5, "Steak", "steakPicture"));
     }
 }
