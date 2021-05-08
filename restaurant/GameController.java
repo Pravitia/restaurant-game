995372ada02bd844e62b0cbd0eacfe961c5c75c2
aaronli-git <li.aaron36@gmail.com>
2021-05-08 16:24:43 -0700

    Added Valve for PlayGameMessage, which updates view from GameStartFrame to RestaurantFrame.

--- restaurant/GameController.java
+++ restaurant/GameController.java
@@ -18,6 +18,7 @@ public class GameController {
 
         // Creates object of each kind of valve
         valves.add(new StartGameValve());
+        valves.add(new PlayGameValve());
     }
 
     public void mainLoop() {
@@ -48,13 +49,30 @@ public class GameController {
             if (message.getClass() != StartGameMessage.class) {
                 return ValveResponse.MISS;
             }
-            // This message is a StartGameMessage message
 
             // Valve response to change view from MainMenuFrame to GameStartFrame
+            // updateToGameStart is a view function that switches frames (Needs to be implemented)
+
             //GameView.updateToGameStart();
             return ValveResponse.EXECUTED;
         }
     }
+
+    private class PlayGameValve implements Valve {
+        public ValveResponse execute(Message message) {
+            if (message.getClass() != PlayGameMessage.class) {
+                return ValveResponse.MISS;
+            }
+
+            // Valve response to change view from GameStartFrame to RestaurantFrame
+            // updateToRestaurantFrame is a view function that switches frames (Needs to be implemented)
+
+            //GameView.updateToRestaurantFrame();
+            return ValveResponse.EXECUTED;
+        }
+    }
+
+
 
     private interface Valve {
         ValveResponse execute(Message message);
