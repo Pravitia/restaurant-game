be1386123117e51822b6b7d461c8490b96fcc26d
JordanMai <jordanmai927@gmail.com>
2021-05-08 23:29:57 -0700

    Update SingleRollMessage.java

--- restaurant/SingleRollMessage.java
+++ restaurant/SingleRollMessage.java
@@ -1,4 +1,22 @@
 package restaurant;
+import java.util.ArrayList;
 
 public class SingleRollMessage implements Message{
+    private Food foodRolled;
+    private Food temp = new Food();
+    private ArrayList<Food> foodList = temp.getLoot();
+    private GachaRoll roll = new GachaRoll(foodList);
+
+    public SingleRollMessage(){
+        
+        foodRolled = roll.getRandomFood();
+
+    }
+
+    public Food getFoodRolled(){
+        return foodRolled;
+    }
+
+
+
 }
