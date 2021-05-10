d1e8ff651ef92c2e0708dca8e9fb991c420bd6df
JordanMai <jordanmai927@gmail.com>
2021-05-09 17:40:26 -0700

    Update MinigameFrame.java

--- restaurant/MinigameFrame.java
+++ restaurant/MinigameFrame.java
@@ -51,7 +51,7 @@ public class MinigameFrame extends JFrame{
         tacoIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentTacoNew.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
         sandwichIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentTurkeySandwichNew.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
         */
-
+        System.out.println(playerFood.size());
         for(int i = 0; i < playerFood.size(); i++){
             System.out.println(i);
            iconList.add(new ImageIcon(new ImageIcon(playerFood.get(i).getPictureLocation()).getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
@@ -129,7 +129,27 @@ public class MinigameFrame extends JFrame{
 
     public void updatePlayerFood(ArrayList<Food> f){
         playerFood = f;
+        updateFrame();
         
+    }
+
+    public void updateFrame(){
+        
+        for(int i = 0; i < playerFood.size(); i++){
+            System.out.println(i);
+           iconList.add(new ImageIcon(new ImageIcon(playerFood.get(i).getPictureLocation()).getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT))); 
+        }
+
+        int xBounds = 0;
+        for(int j = 0; j < iconList.size(); j++){
+            buttonList.add(new JButton(iconList.get(j)));
+            buttonList.get(j).setBounds(xBounds,850,100,100);
+            this.add(buttonList.get(j));
+            xBounds += 100;
+        }
+
+        this.add(minigame);
+
     }
 
 
