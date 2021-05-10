6579bfbb17bbef5ec10327d40c913286ef65887f
Pravitia <pham.anson@gmail.com>
2021-05-09 17:27:11 -0700

    Deleted Leaderboard and removed it from the Main Menu

--- restaurant/MainMenuFrame.java
+++ restaurant/MainMenuFrame.java
@@ -10,15 +10,16 @@ public class MainMenuFrame extends JFrame {
     final int FRAME_HEIGHT = 1000;
     MainMenuComponent mainMenu = new MainMenuComponent();
 
-    JButton playButton, leaderboardButton;
+    JButton playButton;
+            //leaderboardButton;
 
     public MainMenuFrame(BlockingQueue<Message> queue)
     {
         this.queue = queue;
         this.playButton = new JButton("Play");
         playButton.setBounds(700,800,100,30);
-        this.leaderboardButton = new JButton("Leaderboard");
-        leaderboardButton.setBounds(100,800,200,30);
+        //this.leaderboardButton = new JButton("Leaderboard");
+        //leaderboardButton.setBounds(100,800,200,30);
 
         playButton.addActionListener(e -> {
             try {
@@ -30,7 +31,7 @@ public class MainMenuFrame extends JFrame {
         });
 
         this.add(playButton);
-        this.add(leaderboardButton);
+        //this.add(leaderboardButton);
         this.add(mainMenu);
         this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         this.setVisible(true);
