e44fb324b75f2c0c9f6ffd2511dd5a234a676a6e
aaronli-git <li.aaron36@gmail.com>
2021-05-12 18:32:45 -0700

    Moved loot pool into GachaRoll and fixed up SingleRollMessage.

--- restaurant/GachaRoll.java
+++ restaurant/GachaRoll.java
@@ -5,22 +5,31 @@ import java.util.Random;
 
 //gacha model class
 public class GachaRoll {
-    private ArrayList<Food> foodPool;
     private Random rand;
+    private ArrayList<Food> loot;
 
-
-    public GachaRoll(ArrayList<Food> foodList)
+    public GachaRoll()
     {
-        foodPool = foodList;
         rand = new Random();
-
+        loot = new ArrayList<>();
+        loot.add(new Food(25, 5, "Steak", "restaurant/images/transparentSteakNew.png"));
+        loot.add(new Food(18, 4, "Rotisserie Chicken", "restaurant/images/transparentRotisserieChickenNew.png"));
+        loot.add(new Food(1.50, 3, "Hot Dog", "restaurant/images/transparentHotdogNew.png"));
+        loot.add(new Food(15, 4, "Burger", "restaurant/images/transparentBurgerNew.png"));
+        loot.add(new Food(12, 4, "Pepperoni Pizza", "restaurant/images/transparentPepperoniPizzaNew.png"));
+        loot.add(new Food(10, 3, "Turkey Sandwich", "restaurant/images/transparentTurkeySandwichNew.png"));
+        loot.add(new Food(3.5, 3, "Fries", "restaurant/images/transparentFriesNew.png"));
+        loot.add(new Food(14, 4, "Ramen", "restaurant/images/transparentRamenNew.png"));
+        loot.add(new Food(16, 4, "Sushi", "restaurant/images/transparentSushiNew.png"));
+        loot.add(new Food(35, 5, "Strawberry Cake", "restaurant/images/transparentStrawberryCakeNew.png"));
+        loot.add(new Food(1, 3, "Cup Noodle", "restaurant/images/transparentCupNoodleNew.png"));
+        loot.add(new Food(13, 4, "Tacos", "restaurant/images/transparentTacoNew.png"));
+        loot.add(new Food(2, 3, "Soda", "restaurant/images/transparentSodaNew.png"));
+        loot.add(new Food(7, 3, "Milkshake", "restaurant/images/transparentMilkshakeNew.png"));
     }
-
-
-
 
     public Food getRandomFood()
     {
-        return foodPool.get(rand.nextInt(foodPool.size()));
+        return loot.get(rand.nextInt(loot.size()));
     }
 }
