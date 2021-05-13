2263b0e5ab5056cffce62c96fa213fb15eb2fcb3
aaronli-git <li.aaron36@gmail.com>
2021-05-12 21:29:43 -0700

    Added a test actionlistener for a button

--- restaurant/MinigameButtonHandler.java
+++ restaurant/MinigameButtonHandler.java
@@ -7,19 +7,20 @@ import javax.swing.JButton;
 public class MinigameButtonHandler implements ActionListener {
 
     private ArrayList<JButton> buttons;
+    private ArrayList<Food> inventory;
 
-    public MinigameButtonHandler (ArrayList<JButton> buttons) {
+    public MinigameButtonHandler (ArrayList<JButton> buttons, ArrayList<Food> inventory) {
         this.buttons = buttons;
+        this.inventory = inventory;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
         if (e.getSource() == buttons.get(0)) {
-            System.out.println(buttons.get(0).getName());
+            System.out.println(inventory.get(0).getName());
         }
 
-        
     }
     
 }
--- restaurant/MinigameFrame.java
+++ restaurant/MinigameFrame.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.concurrent.BlockingQueue;
 
 
@@ -27,80 +28,8 @@ public class MinigameFrame extends JFrame{
     public MinigameFrame(BlockingQueue<Message> queue)
     {
         this.queue = queue;
-        
 
-        //playerFood.add(new Food(15, 4, "Burger", "restaurant/images/transparentBurgerNew.png"));
-        //playerFood.add(new Food(12, 4, "Pepperoni Pizza", "restaurant/images/transparentPepperoniPizzaNew.png"));
-        //playerFood.add(new Food(10, 3, "Turkey Sandwich", "restaurant/images/transparentTurkeySandwichNew.png"));
-        //Food icons
-        /*
-        burgerIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentBurgerNew.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-        cupNoodleIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentCupNoodleNew.png").getImage().getScaledInstance(70, 100, Image.SCALE_DEFAULT));
-        friesIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentFriesNew.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-        hotdogIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentHotdogNew.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-        milkshakeIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentMilkshakeNew.png").getImage().getScaledInstance(50, 100, Image.SCALE_DEFAULT));
-        pizzaIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentPepperoniPizzaNew.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-        ramenIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentRamenNew.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-        chickenIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentRotisserieChickenNew.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-        sodaIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentSodaNew.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-        steakIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentSteakNew.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-        cakeIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentStrawberryCakeNew.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-        sushiIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentSushiNew.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-        tacoIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentTacoNew.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-        sandwichIcon = new ImageIcon(new ImageIcon("restaurant/images/transparentTurkeySandwichNew.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-        */
-
-        /*
-
-        burgerButton = new JButton(burgerIcon);
-        burgerButton.setBounds(0, 850, 100, 100);
-        cupNoodleButton = new JButton(cupNoodleIcon);
-        cupNoodleButton.setBounds(100, 850, 100, 100);
-        friesButton = new JButton(friesIcon);
-        friesButton.setBounds(200, 850, 100, 100);
-        hotdogButton = new JButton(hotdogIcon);
-        hotdogButton.setBounds(300, 850, 100, 100);
-        milkshakeButton = new JButton(milkshakeIcon);
-        milkshakeButton.setBounds(400, 850, 100, 100);
-        pizzaButton = new JButton(pizzaIcon);
-        pizzaButton.setBounds(500, 850 ,100, 100);
-        ramenButton = new JButton(ramenIcon);
-        ramenButton.setBounds(600, 850, 100, 100);
-        chickenButton = new JButton(chickenIcon);
-        chickenButton.setBounds(700, 850, 100, 100);
-        sodaButton = new JButton(sodaIcon);
-        sodaButton.setBounds(800, 850, 100 ,100);
-        steakButton = new JButton(steakIcon);
-        steakButton.setBounds(900, 850, 100, 100);
-        cakeButton = new JButton(cakeIcon);
-        cakeButton.setBounds(1000, 850, 100, 100);
-        sushiButton = new JButton(sushiIcon);
-        sushiButton.setBounds(1100, 850, 100, 100);
-        tacoButton = new JButton(tacoIcon);
-        tacoButton.setBounds(1200, 850, 100, 100);
-        sandwichButton = new JButton(sandwichIcon);
-        sandwichButton.setBounds(1300, 850, 100, 100);
-
-
-
-        this.add(burgerButton);
-        this.add(cupNoodleButton);
-        this.add(friesButton);
-        this.add(hotdogButton);
-        this.add(milkshakeButton);
-        this.add(pizzaButton);
-        this.add(ramenButton);
-        this.add(chickenButton);
-        this.add(sodaButton);
-        this.add(steakButton);
-        this.add(cakeButton);
-        this.add(sushiButton);
-        this.add(tacoButton);
-        this.add(sandwichButton);
-        */
         this.add(minigame);
-        
-
 
         this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -121,12 +50,8 @@ public class MinigameFrame extends JFrame{
         }
         iconList = new ArrayList<ImageIcon>();
         buttonList = new ArrayList<JButton>();
-        for(int k = 0; k < playerFood.size(); k++){
-            System.out.println(playerFood.get(k).getName());
-        }
-        for(int i = 0; i < playerFood.size(); i++){
 
-           
+        for(int i = 0; i < playerFood.size(); i++){
            iconList.add(new ImageIcon(new ImageIcon(playerFood.get(i).getPictureLocation()).getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT))); 
         }
 
@@ -137,18 +62,22 @@ public class MinigameFrame extends JFrame{
             if(xBounds < 1500){
                 buttonList.add(new JButton(iconList.get(j)));
                 buttonList.get(j).setBounds(xBounds,yBounds,100,100);
-                this.add(buttonList.get(j));
+                //this.add(buttonList.get(j));
                 xBounds += 100;
             }
             else{
                 buttonList.add(new JButton(iconList.get(j)));
                 buttonList.get(j).setBounds(secondXBounds,yBounds+100,100,100);
-                this.add(buttonList.get(j));
+                //this.add(buttonList.get(j));
                 secondXBounds += 100;
 
             }
+        }
 
-
+        MinigameButtonHandler handler = new MinigameButtonHandler(buttonList, playerFood);
+        for (JButton j : buttonList) {
+            j.addActionListener(handler);
+            this.add(j);
         }
 
         this.add(minigame);
