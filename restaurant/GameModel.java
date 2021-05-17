986490925b54b3c390804d0d12f91cddcbe3db5d
Pravitia <pham.anson@gmail.com>
2021-05-17 03:30:00 -0700

    Updated Javadoc

--- restaurant/GameModel.java
+++ restaurant/GameModel.java
@@ -10,35 +10,61 @@ public class GameModel {
     private Customer newCustomer;
     String filepath;
 
+    /**
+     * Constructor for the Game Model
+     * Intialize the Player, GachaRoll, and first Customer
+     */
     public GameModel() {
         this.user = new Player();
         this.roller = new GachaRoll();
         this.newCustomer = new Customer(getUser().getFood());
     }
 
+    /**
+     * Getter method for User
+     * @return user Gets the user
+     */
     public Player getUser()
     {
         return user;
     }
 
+    /**
+     * Getter method for GachaRoll
+     * @return roller Get the roller Gacharoll
+     */
     public GachaRoll getRoller(){
         return roller;
     }
 
-    //for updating food picture in gacha
+    /**
+     * Set method to set the filepath for the image to show
+     * @param s filepath for picture
+     */
     public void setPicture(String s){
         this.filepath = s;
     }
 
+    /**
+     * Getter method for the filepath
+     * @return filepath of the picture of food
+     */
     public String getDisplayFood()
     {
         return filepath;
     }
 
+    /**
+     * Instantiate a new Customer
+     */
     public void makeCustomer(){
         newCustomer = new Customer(user.getFood());
     }
 
+    /**
+     * Getter method for Customer
+     * @return newCustomer is a new customer for the Minigame
+     */
     public Customer getCustomer(){
         return newCustomer;
     }
